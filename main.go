0d529d5c3df97f5adbeea964ee3573d8307a7469
Vladimir Ermakov <vooon341@gmail.com>
2022-11-11 16:05:13 +0100

    implement nova check

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/services"
+	"github.com/gophercloud/utils/openstack/clientconfig"
+	"github.com/jedib0t/go-pretty/v6/table"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
 	"github.com/sensu/sensu-plugin-sdk/sensu"
 )
@@ -12,27 +15,46 @@ import (
 // Config represents the check plugin config.
 type Config struct {
 	sensu.PluginConfig
-	Example string
+	Cloud      string
+	CloudsFile string
+	Service    string
 }
 
 var (
 	plugin = Config{
 		PluginConfig: sensu.PluginConfig{
-			Name:     "{{ .GithubProject }}",
-			Short:    "{{ .Description }}",
-			Keyspace: "sensu.io/plugins/{{ .GithubProject }}/config",
+			Name:     "sensu-go-openstack-service-check",
+			Short:    "Plugin to check OpenStack service states",
+			Keyspace: "sensu.io/plugins/sensu-go-openstack-service-check/config",
 		},
 	}
 
 	options = []sensu.ConfigOption{
 		&sensu.PluginConfigOption[string]{
-			Path:      "example",
-			Env:       "CHECK_EXAMPLE",
-			Argument:  "example",
-			Shorthand: "e",
-			Default:   "",
-			Usage:     "An example string configuration option",
-			Value:     &plugin.Example,
+			Path:      "cloud",
+			Env:       "OS_CLOUD",
+			Argument:  "cloud",
+			Shorthand: "c",
+			Default:   "monitoring",
+			Usage:     "Cloud used to access openstack API",
+			Value:     &plugin.Cloud,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:     "os_config_file",
+			Env:      "OS_CLIENT_CONFIG_FILE",
+			Argument: "os-config-file",
+			Default:  "",
+			Usage:    "Clouds.yaml file path",
+			Value:    &plugin.CloudsFile,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "service",
+			Argument:  "service",
+			Shorthand: "s",
+			Default:   "compute",
+			Allow:     []string{"compute", "network", "volume", "share"},
+			Usage:     "Service to check",
+			Value:     &plugin.Service,
 		},
 	}
 )
@@ -42,11 +64,9 @@ func main() {
 	fi, err := os.Stdin.Stat()
 	if err != nil {
 		fmt.Printf("Error check stdin: %v\n", err)
-		panic(err)
 	}
-	//Check the Mode bitmask for Named Pipe to indicate stdin is connected
+	// Check the Mode bitmask for Named Pipe to indicate stdin is connected
 	if fi.Mode()&os.ModeNamedPipe != 0 {
-		log.Println("using stdin")
 		useStdin = true
 	}
 
@@ -55,13 +75,58 @@ func main() {
 }
 
 func checkArgs(event *corev2.Event) (int, error) {
-	if len(plugin.Example) == 0 {
-		return sensu.CheckStateWarning, fmt.Errorf("--example or CHECK_EXAMPLE environment variable is required")
-	}
 	return sensu.CheckStateOK, nil
 }
 
 func executeCheck(event *corev2.Event) (int, error) {
-	log.Println("executing check with --example", plugin.Example)
-	return sensu.CheckStateOK, nil
+	if plugin.CloudsFile != "" {
+		os.Setenv("OS_CLIENT_CONFIG_FILE", plugin.CloudsFile)
+	}
+
+	opts := &clientconfig.ClientOpts{
+		Cloud: plugin.Cloud,
+	}
+
+	cli, err := clientconfig.NewServiceClient(plugin.Service, opts)
+	if err != nil {
+		return sensu.CheckStateUnknown, err
+	}
+
+	switch plugin.Service {
+	case "compute":
+		return checkCompute(cli)
+
+	default:
+		return sensu.CheckStateUnknown, fmt.Errorf("unsupported service: %s", plugin.Service)
+	}
+}
+
+func checkCompute(cli *gophercloud.ServiceClient) (int, error) {
+	pages, err := services.List(cli, nil).AllPages()
+	if err != nil {
+		return sensu.CheckStateUnknown, err
+	}
+
+	srvs, err := services.ExtractServices(pages)
+	if err != nil {
+		return sensu.CheckStateUnknown, err
+	}
+
+	ret := sensu.CheckStateOK
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"ID", "Binary", "Host", "Zone", "Status", "State", "Updated At"})
+
+	for _, srv := range srvs {
+		t.AppendRow(table.Row{srv.ID, srv.Binary, srv.Host, srv.Zone, srv.Status, srv.State, srv.UpdatedAt})
+
+		if srv.Status == "enabled" && srv.State != "up" {
+			ret = sensu.CheckStateCritical
+		}
+	}
+
+	t.Render()
+
+	return ret, nil
 }
